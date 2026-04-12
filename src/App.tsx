e5644e17a68058d2c25e8cf801c9d509f9cea64d
Nancy Bankson <nbankson3@gmail.com>
2026-04-12 12:30:03 -0500

    Updated routes

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/home" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/projects" element={<ProjectsPage />} />
         </Routes>        
